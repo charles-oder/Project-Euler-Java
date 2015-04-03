ce020883e3556e7a06fbb97374c6a8bd2b370f02
Charles Oder <charles@oder.us>
2015-04-03 16:49:52 -0500

    Problem 1: Removed unused Main class

--- src/main/java/us/oder/euler/Main.java
+++
@@ -1,7 +0,0 @@
-package us.oder.euler;
-
-/**
- * Created by charlesoder on 4/3/15.
- */
-public class Main {
-}
--- src/test/java/us/oder/euler/MainTest.java
+++
@@ -1,7 +0,0 @@
-package us.oder.euler;
-
-import static org.junit.Assert.*;
-
-public class MainTest {
-
-}
