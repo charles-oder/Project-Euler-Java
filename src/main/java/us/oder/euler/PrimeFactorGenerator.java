600945f04d75136444afb92630251e296c10a832
Charles Oder <charles@oder.us>
2015-04-03 19:02:50 -0500

    Problem 2: Added method for retrieving the largest prime factor

--- src/main/java/us/oder/euler/PrimeFactorGenerator.java
+++ src/main/java/us/oder/euler/PrimeFactorGenerator.java
@@ -1,17 +1,52 @@
 package us.oder.euler;
 
+import java.util.*;
+
 /**
  * ProjectEulerJava
  * us.oder.euler
  * Created by charlesoder on 4/3/15.
  */
 public class PrimeFactorGenerator {
-    public static boolean isPrime( int num ) {
+    private final Long value;
+    private SortedSet<Long> list;
+
+    public PrimeFactorGenerator( Long value ) {
+        this.value = value;
+    }
+
+    public static boolean isPrime( Long num ) {
         for ( int i = 2; i <= Math.sqrt( num ); i++ ) {
             if ( num % i == 0 ) {
                 return false;
             }
         }
         return true;
+    }
+
+    public SortedSet<Long> getPrimeFactors() {
+        list = new TreeSet<Long>();
+        for ( Long i = 3L; i <= Math.sqrt( value ); i++ ) {
+            addToListIfPrime( i );
+        }
+        return list;
+    }
+
+    private void addToListIfPrime( Long i ) {
+        if ( isPrimeFactor( i ) ) {
+            list.add( i );
+            if ( !list.contains( value / i ) && isPrime( value / i )) {
+                list.add( value / i );
+            }
+        }
+    }
+
+    private boolean isPrimeFactor( Long i ) {
+        return isPrime( i ) && value % i == 0;
+    }
+
+    public Long getLargestPrimeFactor() {
+        SortedSet<Long> set = getPrimeFactors();
+        return set.last();
     }
 }
--- src/test/java/us/oder/euler/PrimeFactorGeneratorTest.java
+++ src/test/java/us/oder/euler/PrimeFactorGeneratorTest.java
@@ -2,24 +2,64 @@ package us.oder.euler;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PrimeFactorGeneratorTest {
 
     @Test
     public void isPrimeForPrimeNumber(){
-        assertTrue( PrimeFactorGenerator.isPrime( 3 ) );
-        assertTrue( PrimeFactorGenerator.isPrime( 5 ) );
-        assertTrue( PrimeFactorGenerator.isPrime( 7 ) );
-        assertTrue( PrimeFactorGenerator.isPrime( 11 ) );
+        assertTrue( PrimeFactorGenerator.isPrime( 3L ) );
+        assertTrue( PrimeFactorGenerator.isPrime( 5L ) );
+        assertTrue( PrimeFactorGenerator.isPrime( 7L ) );
+        assertTrue( PrimeFactorGenerator.isPrime( 11L ) );
     }
 
     @Test
     public void isPrimeForPrimeNonNumber(){
-        assertFalse( PrimeFactorGenerator.isPrime( 4 ) );
-        assertFalse( PrimeFactorGenerator.isPrime( 6 ) );
-        assertFalse( PrimeFactorGenerator.isPrime( 22 ) );
-        assertFalse( PrimeFactorGenerator.isPrime( 100 ) );
+        assertFalse( PrimeFactorGenerator.isPrime( 4L ) );
+        assertFalse( PrimeFactorGenerator.isPrime( 6L ) );
+        assertFalse( PrimeFactorGenerator.isPrime( 22L ) );
+        assertFalse( PrimeFactorGenerator.isPrime( 100L ) );
+    }
+
+    @Test
+    public void getPrimeFactorListOfPerfectSquare() {
+        Long testValue = 841L;
+        List<Long> expectedList = Arrays.asList( 29L );
+        PrimeFactorGenerator generator = new PrimeFactorGenerator(testValue);
+
+        assertArrayEquals( expectedList.toArray(), generator.getPrimeFactors().toArray() );
+    }
+
+    @Test
+    public void getPrimeFactorListOfProductOfPrimes() {
+        Long testValue = 55L;
+        List<Long> expectedList = Arrays.asList( 5L, 11L );
+        PrimeFactorGenerator generator = new PrimeFactorGenerator(testValue);
+
+        assertArrayEquals( expectedList.toArray(), generator.getPrimeFactors().toArray() );
+    }
+
+    @Test
+    public void getPrimeFactorList() {
+        Long testValue = 13195L;
+        List<Long> expectedList = Arrays.asList( 5L, 7L, 13L, 29L );
+        PrimeFactorGenerator generator = new PrimeFactorGenerator(testValue);
+
+        assertArrayEquals( expectedList.toArray(), generator.getPrimeFactors().toArray() );
+    }
+
+    @Test
+    public void getLargestPrimeFactor() {
+        Long testValue = 600851475143L;
+        PrimeFactorGenerator generator = new PrimeFactorGenerator(testValue);
+
+        assertEquals( 6857L, (long)generator.getLargestPrimeFactor() );
     }
 
 }
